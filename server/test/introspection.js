c65ff36780a6db0dada6562958697c6cdce319f6
Bondi1 <divya.avanna@gmail.com>
2021-06-14 23:39:07 +1000

    Update introspection tests to include more cases

--- server/test/introspection.js
+++ server/test/introspection.js
@@ -1,23 +1,24 @@
 const { expect } = require('chai');
-const { describe, it} = require( 'mocha');
+const { describe, it } = require('mocha');
 const { buildSchema, graphqlSync, introspectionQuery } = require("graphql");
 const sdlString = require('../schema.graphql')
 const graphqlSchemaObj = buildSchema(sdlString);
 
+// Dummy Sample test to check mocha
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
 });
 
 function queryPeople(introspectionQuery) {
-  const result = graphqlSync(graphqlSchemaObj, introspectionQuery );
+  const result = graphqlSync(graphqlSchemaObj, introspectionQuery);
   expect(Object.keys(result)).to.deep.equal(['data']);
-  console.log("=====Result====")
-  console.log(result);
+  // console.log("=====Result====")
+  // console.log(result);
   return result.data;
 }
 
@@ -40,445 +41,216 @@ describe('All People Introspection Tests', () => {
       expect(data).to.deep.equal({
         __schema: {
           types: [
-            {
-              "name": "Film"
-            },
-            {
-              "name": "String"
-            },
-            {
-              "name": "Int"
-            },
-            {
-              "name": "ID"
-            },
-            {
-              "name": "PeopleFilmsEdge"
-            },
-            {
-              "name": "PeopleFilmsConnection"
-            },
-            {
-              "name": "Person"
-            },
-            {
-              "name": "AllPeople"
-            },
-            {
-              "name": "Review"
-            },
-            {
-              "name": "AllReviews"
-            },
-            {
-              "name": "Query"
-            },
-            {
-              "name": "Mutation"
-            },
-            {
-              "name": "AddReview"
-            },
-            {
-              "name": "Boolean"
-            },
-            {
-              "name": "__Schema"
-            },
-            {
-              "name": "__Type"
-            },
-            {
-              "name": "__TypeKind"
-            },
-            {
-              "name": "__Field"
-            },
-            {
-              "name": "__InputValue"
-            },
-            {
-              "name": "__EnumValue"
-            },
-            {
-              "name": "__Directive"
-            },
-            {
-              "name": "__DirectiveLocation"
-            }
+            { "name": "Film" },
+            { "name": "String" },
+            { "name": "Int" },
+            { "name": "ID" },
+            { "name": "PeopleFilmsEdge" },
+            { "name": "PeopleFilmsConnection" },
+            { "name": "Person" },
+            { "name": "AllPeople" },
+            { "name": "Review" },
+            { "name": "AllReviews" },
+            { "name": "Query" },
+            { "name": "Mutation" },
+            { "name": "AddReview" },
+            { "name": "Boolean" },
+            { "name": "__Schema" },
+            { "name": "__Type" },
+            { "name": "__TypeKind" },
+            { "name": "__Field" },
+            { "name": "__InputValue" },
+            { "name": "__EnumValue" },
+            { "name": "__Directive" },
+            { "name": "__DirectiveLocation" }
           ],
         },
       });
     });
+
+    it('Allows querying the schema for query type', () => {
+      const data = queryPeople(`
+        {
+          __schema {
+            queryType {
+              name
+            }
+          }
+        }
+      `);
+
+      expect(data).to.deep.equal({
+        __schema: {
+          queryType: {
+            name: 'Query',
+          },
+        },
+      });
+    });
+
+    it('Allows querying the schema for a specific type', () => {
+      const data = queryPeople(`
+        {
+          __type(name: "Person") {
+            name
+          }
+        }
+      `);
+
+      expect(data).to.deep.equal({
+        __type: {
+          name: 'Person',
+        },
+      });
+    });
+
+    it('Allows querying the schema for an object kind', () => {
+      const data = queryPeople(`
+        {
+          __type(name: "Person") {
+            name
+            kind
+          }
+        }
+      `);
+
+      expect(data).to.deep.equal({
+        __type: {
+          name: 'Person',
+          kind: 'OBJECT',
+        },
+      });
+    });
+
+    it('Allows querying the schema for nested object fields', () => {
+      const data = queryPeople(`
+        {
+          __type(name: "Person") {
+            name
+            fields {
+              name
+              type {
+                name
+                kind
+                ofType {
+                  name
+                  kind
+                }
+              }
+            }
+          }
+        }
+      `);
+
+      expect(data).to.deep.equal(
+        {
+          "__type": {
+            "name": "Person",
+            "fields": [
+              {
+                "name": "id",
+                "type": {
+                  "name": null,
+                  "kind": "NON_NULL",
+                  "ofType": {
+                    "name": "ID",
+                    "kind": "SCALAR"
+                  }
+                }
+              },
+              {
+                "name": "name",
+                "type": {
+                  "name": "String",
+                  "kind": "SCALAR",
+                  "ofType": null
+                }
+              },
+              {
+                "name": "birthYear",
+                "type": {
+                  "name": "String",
+                  "kind": "SCALAR",
+                  "ofType": null
+                }
+              },
+              {
+                "name": "eyeColor",
+                "type": {
+                  "name": "String",
+                  "kind": "SCALAR",
+                  "ofType": null
+                }
+              },
+              {
+                "name": "gender",
+                "type": {
+                  "name": "String",
+                  "kind": "SCALAR",
+                  "ofType": null
+                }
+              },
+              {
+                "name": "hairColor",
+                "type": {
+                  "name": "String",
+                  "kind": "SCALAR",
+                  "ofType": null
+                }
+              },
+              {
+                "name": "height",
+                "type": {
+                  "name": "Int",
+                  "kind": "SCALAR",
+                  "ofType": null
+                }
+              },
+              {
+                "name": "mass",
+                "type": {
+                  "name": "Int",
+                  "kind": "SCALAR",
+                  "ofType": null
+                }
+              },
+              {
+                "name": "skinColor",
+                "type": {
+                  "name": "String",
+                  "kind": "SCALAR",
+                  "ofType": null
+                }
+              },
+              {
+                "name": "created",
+                "type": {
+                  "name": "String",
+                  "kind": "SCALAR",
+                  "ofType": null
+                }
+              },
+              {
+                "name": "edited",
+                "type": {
+                  "name": "String",
+                  "kind": "SCALAR",
+                  "ofType": null
+                }
+              },
+              {
+                "name": "filmConnection",
+                "type": {
+                  "name": "PeopleFilmsConnection",
+                  "kind": "OBJECT",
+                  "ofType": null
+                }
+              }
+            ]
+          }
+        }
+      );
+    });
+
   });
 });
-
-
-
-
-// import { expect } from 'chai';
-// import { describe, it } from 'mocha';
-
-// import { graphqlSync } from '../graphql';
-
-// import { StarWarsSchema } from './starWarsSchema';
-
-// function queryStarWars(source) {
-//   const result = graphqlSync({ schema: StarWarsSchema, source });
-//   expect(Object.keys(result)).to.deep.equal(['data']);
-//   return result.data;
-// }
-
-// describe('Star Wars Introspection Tests', () => {
-//   describe('Basic Introspection', () => {
-//     it('Allows querying the schema for types', () => {
-//       const data = queryStarWars(`
-//         {
-//           __schema {
-//             types {
-//               name
-//             }
-//           }
-//         }
-//       `);
-
-//       // Include all types used by StarWars schema, introspection types and
-//       // standard directives. For example, `Boolean` is used in `@skip`,
-//       // `@include` and also inside introspection types.
-//       expect(data).to.deep.equal({
-//         __schema: {
-//           types: [
-//             { name: 'Human' },
-//             { name: 'Character' },
-//             { name: 'String' },
-//             { name: 'Episode' },
-//             { name: 'Droid' },
-//             { name: 'Query' },
-//             { name: 'Boolean' },
-//             { name: '__Schema' },
-//             { name: '__Type' },
-//             { name: '__TypeKind' },
-//             { name: '__Field' },
-//             { name: '__InputValue' },
-//             { name: '__EnumValue' },
-//             { name: '__Directive' },
-//             { name: '__DirectiveLocation' },
-//           ],
-//         },
-//       });
-//     });
-
-//     it('Allows querying the schema for query type', () => {
-//       const data = queryStarWars(`
-//         {
-//           __schema {
-//             queryType {
-//               name
-//             }
-//           }
-//         }
-//       `);
-
-//       expect(data).to.deep.equal({
-//         __schema: {
-//           queryType: {
-//             name: 'Query',
-//           },
-//         },
-//       });
-//     });
-
-//     it('Allows querying the schema for a specific type', () => {
-//       const data = queryStarWars(`
-//         {
-//           __type(name: "Droid") {
-//             name
-//           }
-//         }
-//       `);
-
-//       expect(data).to.deep.equal({
-//         __type: {
-//           name: 'Droid',
-//         },
-//       });
-//     });
-
-//     it('Allows querying the schema for an object kind', () => {
-//       const data = queryStarWars(`
-//         {
-//           __type(name: "Droid") {
-//             name
-//             kind
-//           }
-//         }
-//       `);
-
-//       expect(data).to.deep.equal({
-//         __type: {
-//           name: 'Droid',
-//           kind: 'OBJECT',
-//         },
-//       });
-//     });
-
-//     it('Allows querying the schema for an interface kind', () => {
-//       const data = queryStarWars(`
-//         {
-//           __type(name: "Character") {
-//             name
-//             kind
-//           }
-//         }
-//       `);
-
-//       expect(data).to.deep.equal({
-//         __type: {
-//           name: 'Character',
-//           kind: 'INTERFACE',
-//         },
-//       });
-//     });
-
-//     it('Allows querying the schema for object fields', () => {
-//       const data = queryStarWars(`
-//         {
-//           __type(name: "Droid") {
-//             name
-//             fields {
-//               name
-//               type {
-//                 name
-//                 kind
-//               }
-//             }
-//           }
-//         }
-//       `);
-
-//       expect(data).to.deep.equal({
-//         __type: {
-//           name: 'Droid',
-//           fields: [
-//             {
-//               name: 'id',
-//               type: { name: null, kind: 'NON_NULL' },
-//             },
-//             {
-//               name: 'name',
-//               type: { name: 'String', kind: 'SCALAR' },
-//             },
-//             {
-//               name: 'friends',
-//               type: { name: null, kind: 'LIST' },
-//             },
-//             {
-//               name: 'appearsIn',
-//               type: { name: null, kind: 'LIST' },
-//             },
-//             {
-//               name: 'secretBackstory',
-//               type: { name: 'String', kind: 'SCALAR' },
-//             },
-//             {
-//               name: 'primaryFunction',
-//               type: { name: 'String', kind: 'SCALAR' },
-//             },
-//           ],
-//         },
-//       });
-//     });
-
-//     it('Allows querying the schema for nested object fields', () => {
-//       const data = queryStarWars(`
-//         {
-//           __type(name: "Droid") {
-//             name
-//             fields {
-//               name
-//               type {
-//                 name
-//                 kind
-//                 ofType {
-//                   name
-//                   kind
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       `);
-
-//       expect(data).to.deep.equal({
-//         __type: {
-//           name: 'Droid',
-//           fields: [
-//             {
-//               name: 'id',
-//               type: {
-//                 name: null,
-//                 kind: 'NON_NULL',
-//                 ofType: {
-//                   name: 'String',
-//                   kind: 'SCALAR',
-//                 },
-//               },
-//             },
-//             {
-//               name: 'name',
-//               type: {
-//                 name: 'String',
-//                 kind: 'SCALAR',
-//                 ofType: null,
-//               },
-//             },
-//             {
-//               name: 'friends',
-//               type: {
-//                 name: null,
-//                 kind: 'LIST',
-//                 ofType: {
-//                   name: 'Character',
-//                   kind: 'INTERFACE',
-//                 },
-//               },
-//             },
-//             {
-//               name: 'appearsIn',
-//               type: {
-//                 name: null,
-//                 kind: 'LIST',
-//                 ofType: {
-//                   name: 'Episode',
-//                   kind: 'ENUM',
-//                 },
-//               },
-//             },
-//             {
-//               name: 'secretBackstory',
-//               type: {
-//                 name: 'String',
-//                 kind: 'SCALAR',
-//                 ofType: null,
-//               },
-//             },
-//             {
-//               name: 'primaryFunction',
-//               type: {
-//                 name: 'String',
-//                 kind: 'SCALAR',
-//                 ofType: null,
-//               },
-//             },
-//           ],
-//         },
-//       });
-//     });
-
-//     it('Allows querying the schema for field args', () => {
-//       const data = queryStarWars(`
-//         {
-//           __schema {
-//             queryType {
-//               fields {
-//                 name
-//                 args {
-//                   name
-//                   description
-//                   type {
-//                     name
-//                     kind
-//                     ofType {
-//                       name
-//                       kind
-//                     }
-//                   }
-//                   defaultValue
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       `);
-
-//       expect(data).to.deep.equal({
-//         __schema: {
-//           queryType: {
-//             fields: [
-//               {
-//                 name: 'hero',
-//                 args: [
-//                   {
-//                     defaultValue: null,
-//                     description:
-//                       'If omitted, returns the hero of the whole saga. If provided, returns the hero of that particular episode.',
-//                     name: 'episode',
-//                     type: {
-//                       kind: 'ENUM',
-//                       name: 'Episode',
-//                       ofType: null,
-//                     },
-//                   },
-//                 ],
-//               },
-//               {
-//                 name: 'human',
-//                 args: [
-//                   {
-//                     name: 'id',
-//                     description: 'id of the human',
-//                     type: {
-//                       kind: 'NON_NULL',
-//                       name: null,
-//                       ofType: {
-//                         kind: 'SCALAR',
-//                         name: 'String',
-//                       },
-//                     },
-//                     defaultValue: null,
-//                   },
-//                 ],
-//               },
-//               {
-//                 name: 'droid',
-//                 args: [
-//                   {
-//                     name: 'id',
-//                     description: 'id of the droid',
-//                     type: {
-//                       kind: 'NON_NULL',
-//                       name: null,
-//                       ofType: {
-//                         kind: 'SCALAR',
-//                         name: 'String',
-//                       },
-//                     },
-//                     defaultValue: null,
-//                   },
-//                 ],
-//               },
-//             ],
-//           },
-//         },
-//       });
-//     });
-
-//     it('Allows querying the schema for documentation', () => {
-//       const data = queryStarWars(`
-//         {
-//           __type(name: "Droid") {
-//             name
-//             description
-//           }
-//         }
-//       `);
-
-//       expect(data).to.deep.equal({
-//         __type: {
-//           name: 'Droid',
-//           description: 'A mechanical creature in the Star Wars universe.',
-//         },
-//       });
-//     });
-//   });
-// });
